bfc14eba49b4c529dd04c83cb07f60dddc0bacba
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-01-18 00:28:23 +0600

    add: footer css + html

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,28 @@
-<footer>Super footer.</footer>
+<footer class="footer">
+    <div class="wrapper">
+    <section class="footer__container">
+    <!--#footer-->
+      <div class="footer__container__list">
+        <section class="footer__container__list__characteristic">
+          <div class="footer__container__list__characteristic__item">ROTATION TIME</div>
+          <div class="footer__container__list__characteristic__content">17.2 hours</div>
+        </section>
+
+        <section class="footer__container__list__characteristic">
+          <div class="footer__container__list__characteristic__item">REVOLUTION TIME</div>
+          <div class="footer__container__list__characteristic__content">11.86 years</div>
+        </section>
+
+        <section class="footer__container__list__characteristic">
+          <div class="footer__container__list__characteristic__item">Radius</div>
+          <div class="footer__container__list__characteristic__content">69,911 km</div>
+        </section>
+
+        <section class="footer__container__list__characteristic">
+          <div class="footer__container__list__characteristic__item">AVERAGE TEMP.</div>
+          <div class="footer__container__list__characteristic__content">-108°c</div>
+        </section>
+      </div>
+    </section>
+    </div>
+</footer>	  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
-    import Footer from '$lib/components/Footer.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '$lib/styles/default.css';
 	import '$lib/styles/header.css';
-    import '$lib/styles/main.css';
+	import '$lib/styles/main.css';
 	import '$lib/styles/footer.css';
-
-
-
 </script>
-
-
 
 <Header />
 
