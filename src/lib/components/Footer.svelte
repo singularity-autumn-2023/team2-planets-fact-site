315f3860a8c60933e743f0e5ecf37d65af7ad4cc
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-01-21 21:06:54 +0600

    feat: convertor unit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,48 +1,72 @@
-<!-- Footer.svelte -->
-
 <script>
-  import { page } from '$app/stores';
-  const pager = $page;
+	import { page } from '$app/stores';
+	const pager = $page;
 
-  let headerMenu = pager.data.superPlanets.then((result) => {
-      return result;
-  });
+	let headerMenu = pager.data.superPlanets.then((result) => {
+		return result;
+	});
+// {pager.data.superPlanets.rotation} 
+// {pager.data.superPlanets.revolution}
+// {pager.data.superPlanets.radius}
+// {pager.data.superPlanets.temperature}
+
+	let currentDate = new Date();
+
+	let rotationTimestamp = new Date(5063040000);
+	let h = rotationTimestamp.getHours();
+	let mm = rotationTimestamp.getMinutes();
+	let outputRotation = `${h}.${mm} HOURS`;
+
+	let revolutionDate = new Date(7600608000);
+	let y = revolutionDate.getFullYear();
+	let month = revolutionDate.getFullYear();
+	let yearsDiff = currentDate.getFullYear() - revolutionDate.getFullYear();
+	let monthsDiff = currentDate.getMonth() - revolutionDate.getMonth();
+
+	if (monthsDiff < 0) {
+		yearsDiff = yearsDiff - 1;
+		monthsDiff = monthsDiff + 12;
+	}
+	let outputREVOLUTION = `${yearsDiff}.${monthsDiff} years`;
+
+	let radiusInMeters = 2439700;
+	let outputRadiusInKilometers = radiusInMeters / 1000 + ' km';
+
+	let temperatureInFahrenheit = 430;
+	let outputTemperatureInCelsius = Math.round(((temperatureInFahrenheit - 32) * 5) / 9) + '°c';
+
+	//console.log(outputREVOLUTION);
 </script>
 
-
 <footer class="footer">
-    <div class="wrapper">
-    <section class="footer__container">
-    <!--#footer-->
-      <div class="footer__container__list">
-        <section class="footer__container__list__characteristic">
-          <div class="footer__container__list__characteristic__item">ROTATION TIME</div>
-          <div class="footer__container__list__characteristic__content">{pager.data.superPlanets.rotation} 
-            {#await headerMenu}
-              <p>...waiting</p>
-            {:then rot}
-              <p>{rot}</p>
-            {:catch error}
-              <p style="color: red">{error.message}</p>
-            {/await} hours
-          </div>
-        </section>
+	<div class="wrapper">
+		<section class="footer__container">
+			<!--#footer-->
+			<div class="footer__container__list">
+				<section class="footer__container__list__characteristic">
+					<div class="footer__container__list__characteristic__item">ROTATION TIME</div>
+					<div class="footer__container__list__characteristic__content">{outputRotation}</div>
+				</section>
 
-        <section class="footer__container__list__characteristic">
-          <div class="footer__container__list__characteristic__item">REVOLUTION TIME</div>
-          <div class="footer__container__list__characteristic__content">{pager.data.superPlanets.revolution} years</div>
-        </section>
+				<section class="footer__container__list__characteristic">
+					<div class="footer__container__list__characteristic__item">REVOLUTION TIME</div>
+					<div class="footer__container__list__characteristic__content">{outputREVOLUTION}</div>
+				</section>
 
-        <section class="footer__container__list__characteristic">
-          <div class="footer__container__list__characteristic__item">Radius</div>
-          <div class="footer__container__list__characteristic__content">{pager.data.superPlanets.radius} km</div>
-        </section>
+				<section class="footer__container__list__characteristic">
+					<div class="footer__container__list__characteristic__item">Radius</div>
+					<div class="footer__container__list__characteristic__content">
+						{outputRadiusInKilometers}
+					</div>
+				</section>
 
-        <section class="footer__container__list__characteristic">
-          <div class="footer__container__list__characteristic__item">AVERAGE TEMP.</div>
-          <div class="footer__container__list__characteristic__content">{pager.data.superPlanets.temperature}c</div>
-        </section>
-      </div>
-    </section>
-    </div>
-</footer>	  
+				<section class="footer__container__list__characteristic">
+					<div class="footer__container__list__characteristic__item">AVERAGE TEMP.</div>
+					<div class="footer__container__list__characteristic__content">
+						{outputTemperatureInCelsius}
+					</div>
+				</section>
+			</div>
+		</section>
+	</div>
+</footer>
